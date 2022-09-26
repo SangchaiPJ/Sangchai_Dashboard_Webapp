00dab4fafbee61dc777a8296858a20fcf671c569
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-09-26 20:37:18 +0700

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ import datetime
 import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from time import sleep
 
 st.set_page_config(layout="wide")   # Set the layout of web app as wide mode
@@ -297,15 +298,17 @@ def page3():
             return slope * sale_scat + intercept   # y = mx + c
 
         mymodel = list(map(myfunc, sale_scat))    
-
+        fig2 = make_subplots(rows=1, cols=1,
+                      specs=[[{"secondary_y": True}]])
         fig2 = px.scatter(df_2017_2020, y="Profit", x="Sales")
         fig2.update_traces(textfont_size=16, hovertemplate="Sale: %{x}\n Profit: %{y}", marker_color = df_2017_2020['Color'])
+        fig2.add_trace(secondary_y = True)
         fig2.update_xaxes(tickangle=0)
         fig2.update_layout(width=550, height=350, bargap=0.4, font_family = "sans-serif", font_size = 16,
                            plot_bgcolor = "#F2F2F2", barmode = 'stack',
                            margin=dict(l=10, r=10, t=10, b=10),
                            legend=dict(yanchor="top", xanchor="left", x=0.01))  
-        fig2.update_traces(
+        fig2.add_trace(
             go.Scatter(
                 x=df_2017_2020['Sales'],
                 y=mymodel,
@@ -319,7 +322,8 @@ def page3():
                     color='#CF325B',
                     width=4,
                     )
-                ),        
+                ),
+                secondary_y = False        
             )
         )
         st.plotly_chart(fig2, use_container_width=True)
