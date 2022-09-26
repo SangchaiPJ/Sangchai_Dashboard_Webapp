76caab52fee796a33d29f405dd1e847df3214aeb
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-09-26 19:20:38 +0700

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def page2():
     st.write('')
     st.write("""I\'m Sangchai Paijit. You can call me Babe. I\'m 26 years old.\
         I graduated with a bachelor's degree in electronic engineering from Suranaree University of Technology.\
-        Also, I just have graduated with a master\'s degree of electrical engineering from Suranaree University of Technology.\
+        Also, I just have graduated with a master's degree in electrical engineering from Suranaree University of Technology.\
         I have an experiences with the digital holography (DH) during a master\'s degree studeis.\
         For a brief explanation, we store the hologram signal of the particle by using the CCD sensor with the resolution of 640×480 pixel.
         Next, we import the data to MATLAB then analyze the hologram signal by calculating the numerical reconstruction by using\
