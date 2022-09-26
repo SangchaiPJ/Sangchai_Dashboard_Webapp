0a988c5cfdede2faa04451a59fd9884c5aea2ba4
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-09-26 20:11:17 +0700

    Profit forecast (hover) in linear regression

--- streamlit_app.py
+++ streamlit_app.py
@@ -309,7 +309,7 @@ def page3():
                 y=mymodel,
                 mode="markers+lines",
                 name="Linear Regression Forecast<br>with R-squared = 0.501",
-                hovertemplate="%{Profit forecast: %{y}",
+                hovertemplate="Profit forecast: %{y}",
                 marker=dict(
                 color='#CF325B',
                 size=2,
