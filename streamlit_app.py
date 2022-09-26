f4ece31003e91621970ddc84c4b81a060adb9b51
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-09-26 20:29:49 +0700

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -299,7 +299,7 @@ def page3():
         mymodel = list(map(myfunc, sale_scat))    
 
         fig2 = px.scatter(df_2017_2020, y="Profit", x="Sales")
-        fig2.add_traces(textfont_size=16, hovertemplate="Sale: %{x}\n Profit: %{y}", marker_color = df_2017_2020['Color'])
+        fig2.update_traces(textfont_size=16, hovertemplate="Sale: %{x}\n Profit: %{y}", marker_color = df_2017_2020['Color'])
         fig2.update_xaxes(tickangle=0)
         fig2.update_layout(width=550, height=350, bargap=0.4, font_family = "sans-serif", font_size = 16,
                            plot_bgcolor = "#F2F2F2", barmode = 'stack',
