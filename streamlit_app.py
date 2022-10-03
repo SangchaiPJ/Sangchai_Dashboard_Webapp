0942a8daa629b84fa596c9e8b646572f09800bd3
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-10-04 01:25:02 +0700

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -562,7 +562,8 @@ def page3():
         fig4 = px.pie(cat_sale, labels='Category', values='Sales', names='Category')
         fig4.update_traces(hoverinfo='label+percent', textinfo='label+percent', textposition='inside', textfont_size=16, textfont_color = "White")
         fig4.update_layout(font_family="sans-serif", plot_bgcolor="#F2F2F2", legend = dict(font = dict(family = "sans-serif", size = 16)),
-                  legend_title = dict(font = dict(family = "sans-serif", size = 16)))
+                  legend_title = dict(font = dict(family = "sans-serif", size = 16)),
+                  margin=dict(l=10, r=10, t=10, b=10))
         st.plotly_chart(fig4, use_container_width=True)
     
     #### Sales by Sub-Cat ####
@@ -573,8 +574,7 @@ def page3():
         fig5 = px.pie(subcat_sale, labels='Sub-Category', values='Sales', names='Sub-Category')
         fig5.update_traces(hoverinfo='label+percent', textinfo='label+percent', textposition='inside', textfont_size=16, textfont_color = "White")
         fig5.update_layout(font_family="sans-serif", plot_bgcolor="#F2F2F2", legend = dict(font = dict(family = "sans-serif", size = 16)),
-                  legend_title = dict(font = dict(family = "sans-serif", size = 16)),
-                  margin=dict(l=10, r=10, t=10, b=10))
+                  legend_title = dict(font = dict(family = "sans-serif", size = 16)),)
         st.plotly_chart(fig5, use_container_width=True)
 
     #### Sales by Segment and Sub-Cat ####
