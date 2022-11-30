f7625ae3494b7f4aa44ff7705b9ca871bdc040d9
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-12-01 01:34:40 +0700

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -664,7 +664,7 @@ def page4():
     ### Insert the image of XYZ banking web page ###
     with row_1_col_1:
         st.markdown(
-            f'<div class = "imgautomated"; style = "margin-top: 40px;">'
+            f'<div class = "imgautomated"; style = "margin-top: 50px;">'
             f'<img src="data:image/gif;base64,{data_url7}";  style = "border-radius:15px">'
             f'</div>',
             unsafe_allow_html=True,
