aab74ba5067928f457b1fc88558797e7ce5796fb
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-12-03 02:22:37 +0700

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -742,7 +742,7 @@ def page4():
     st.write("""In this test case, the condition to confirm that any name can login is that when user login,\
         they can see an ""Account Number : " after login.""")
 
-    st.write("##### TC-001 Verify that user can login by choosing any name from a dropdown list")
+    st.write("##### TC-002 Verify that user can deposit money after login")
     st.write("## ⌛ New contents coming soon...")
 
 
