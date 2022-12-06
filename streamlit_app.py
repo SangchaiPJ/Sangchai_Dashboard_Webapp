204ec5cd4421872bd7ab63a62a9a99cf55ccb2bc
Sangchai P <100411149+SangchaiPJ@users.noreply.github.com>
2022-12-06 17:17:20 +0700

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -835,6 +835,7 @@ def page4():
         st.write('')
         st.write('')
         st.write('')
+        st.write('')
         tc004 = Image.open("asset/tc004.png")
         st.image(tc004)
         
